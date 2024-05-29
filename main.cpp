ca7f47086dbfbb7132861d8bef3465909b760132
Wang, Yang <yang4.wang@intel.com>
2024-05-29 16:04:27 +0800

    Update.

--- main.cpp
+++ main.cpp
@@ -4,13 +4,10 @@
 #include "monitors/presenter.h"
 int main(int argc, char* argv[]) {
     Presenter presenter("C");
-    int index = 0;
-    while (index++ < 10) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-        presenter.collect();
-        for (auto item : presenter.reportMeans()) {
-            std::cout << "\n===\n" << item << "\n===" << std::endl;
-        }
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+    presenter.collect();
+    for (auto item : presenter.reportMeans()) {
+        std::cout << item << std::endl;
     }
     return 0;
 }
